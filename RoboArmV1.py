663fea421d019511693eed1708f05fdf16b069b5
Issa Beekun <goodflyerfye@gmail.com>
2014-04-19 15:40:18 -0700

    Robot Arm script now capable of plotting image as well as predicting servo angles that correspond to those points. Removed unnecessary files.

--- RoboArmV1.py
+++ RoboArmV1.py
@@ -4,8 +4,7 @@
  
 # Consider making the path thingy a depth first search
  
-# Drawing algorithm
-
+# **image parsing algorithm**
 # Load picture/image
 # Starting a pointer in upper left corner, check if current pixel is filled
 # If so:
@@ -22,6 +21,14 @@
 #  
 #  arms are 1.25" each
 
+# **arm drawing algorithm**
+# Take the list of lines that have been generated from the parsed image
+# For each line:
+#   - Move to the angles for servo1 and servo2 that define first point
+#   - Move servo3 (attached to the drawing implement) so it just touches the paper
+#   - Iterate through all points/servo angles for that line
+#   - After last line is finished, stay at current point but move servo3 to lift drawing implement off paper
+
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
@@ -30,49 +37,59 @@ import matplotlib.image as mpimg
 particles_size = 3      #Size of particles displayed
 scale_factor = 1.0     #Scale the image dimensions
 threshhold = 0.5       #Threshold brightness value, determines which points to print. 
-                                    #Higher value means lighter colors are printed.
+                                  #Higher value means lighter colors are printed.
 max_recursion = 300  #Maximum length of a continuous line.
-frames = 300            #The total length of time the animation runs for
-interval = 1           #The interval between animation actions
+frames = 300              #The total length of time the animation runs for
+interval = 1                #The interval between animation actions
+
+arm1 = 100.0             #The length of arm 1 
+arm2 = 100.0             #The length of arm 2 
+
+theta1pos= 0.0       #The angle of servo1
+theta2pos= 0.0       #The angle of servo2
+theta3pos= 0.0       #The angle of servo3
 
 image_X = []            #Used to store the x-coordinates of each particle
 image_Y = []            #Used to store the y-coordinates of each particle
 
-parse_list = []              #data structure to store list of lines that compose the image
+parse_list = []           #data structure to store list of lines that compose the image
 line_list = []              #data structure to contain list of line objects in addition to list of lines that compose the image
+angle_list = []           #data structure to store servo angles for robotic arm
 
-image_location = 'images\\bernese.png'
+image_location = 'images\\bernese2.png'
 #read the image specified by the global variables
 read_image = mpimg.imread(image_location)
 
-# First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
-ax = plt.axes(xlim=(-1, len(read_image[0])/scale_factor) , ylim=(0,  1+ len(read_image)/scale_factor))
-# ax = plt.axes(xlim=(-1, 100) , ylim=(0,  100))
-
-# line, = ax.plot([], [], lw=2)
-
-# parse_list, = ax.plot([], [], lw=2)
 
 # initialization function: plot the background of each frame
 def init():
-    print "Started init"
+    # print "Started init"
+    global ax
+    
+    # First set up the figure, the axis, and the plot element we want to animate
+    ax = plt.axes(xlim=(-1, len(read_image[0])/scale_factor) , ylim=(0,  1+ len(read_image)/scale_factor))
+
+    # line, = ax.plot([], [], lw=2)
+
+    for x_list, y_list in parse_list:
+        anim_line, = ax.plot([], [], lw=2)
+
+        line_list.append((x_list, y_list, anim_line))
+    
     # line.set_data([], [])
     
     return_line = []
     for x_list, y_list, anim_line in line_list:
         anim_line.set_data(x_list, y_list)
-        # anim_line.set_data([], [])
         return_line.append(anim_line)
     
-    print "Finished init"
+    # print "Finished init"
     return tuple(return_line)
     # return line, 
-    # return particles,
 
 # animation function.  This is called sequentially
 def animate(i):
-    print i
     return_line = []
     
     # global image_X, image_Y
@@ -82,10 +99,11 @@ def animate(i):
         anim_line.set_data(x_list[0:i], y_list[0:i])
         return_line.append(anim_line)
         
-    # return line, return_line,
     return tuple(return_line)
     # return line,
     
+# parses a pre-defined image into a list of lines as defined by x, y groupings
+# (the x-coordinates are stored on a list and the y coordinates are stored on a separate list)
 def parse_image():
     global scale_factor, threshhold, read_image
     
@@ -112,6 +130,8 @@ def parse_image():
             x_index += 1
         y_index += 1
 
+# recursively determine if the points surrounding the current point should be added to current line
+# uses a pre-defined thresh hold limit
 def parse_recursive(input_y, input_x, recursion_level, line_X, line_Y):
     global scale_factor, threshhold, read_image
     
@@ -125,14 +145,12 @@ def parse_recursive(input_y, input_x, recursion_level, line_X, line_Y):
     line_Y.append((len(read_image) - input_y)/scale_factor)
     
     #erase from image array
-    # print input_y, input_x, read_image[input_y][input_x]
     read_image[input_y][input_x] = [1, 1, 1, 1]
     
     x_min = 0
     x_max = len(read_image[0])
     y_min = 0
     y_max = len(read_image)
-    # print 'Y',y_min,y_max,'/X',x_min, x_max, "\t", input_y,input_x
     
     if(recursion_level < max_recursion):
         recursion_level += 1
@@ -173,33 +191,64 @@ def parse_recursive(input_y, input_x, recursion_level, line_X, line_Y):
             parse_recursive(input_y, input_x - 1, recursion_level, line_X, line_Y)
         # else:
             # print "none"
-        
 
+# takes as input the parse_list containing lines that compose the parsed image
+# generates a list of angles for the three servos of the robot arm
+def generate_arm_angles():
+    #off the wall random guesses for what up and down are for the robot arm
+    down  = 0.87
+    up = 0.20
+    
+    # angle_list
+    for x_list, y_list in parse_list:
+        x_init = x_list[0]
+        y_init = y_list[0]
+        theta1_final = 0
+        theta2_final = 0
+        
+        # print x_init, y_init, arm1, arm2
+        # print (arm1 + arm2)**2
+        # print (x_init**2 + y_init**2)
+        # print (arm1 - arm2)**2
+        # print ((arm1 + arm2)**2 - (x_init**2 + y_init**2)) 
+        # print ((x_init**2 + y_init**2) - (arm1 - arm2)**2)
+        # np.sqrt( ((arm1 + arm2)**2 - (x_init**2 + y_init**2)) / ((x_init**2 + y_init**2) - (arm1 - arm2)**2) )
+    
+        #initially, pen is over first point but does not touch it (such a tease)
+        theta3pos = up
+        theta2pos = 2.0 * np.arctan(np.sqrt( ((arm1 + arm2)**2 - (x_init**2 + y_init**2)) / ((x_init**2 + y_init**2) - (arm1 - arm2)**2) ))
+        theta1pos = np.arctan2(y_init,x_init) - np.arctan2(arm2*np.sin(theta2pos), arm1 + arm2 * np.cos(theta2pos))
+
+        angle_list.append([theta1pos, theta2pos, theta3pos])
+                
+        for point_y in y_list:
+            for point_x in x_list:
+                theta3pos = down
+                theta2pos = 2.0 * np.arctan(np.sqrt( ((arm1 + arm2)**2 - (point_x**2 + point_y**2)) / ((point_x**2 + point_y**2) - (arm1 - arm2)**2) ))
+                theta1pos = np.arctan2(point_y,point_x) - np.arctan2(arm2*np.sin(theta2pos), arm1 + arm2 * np.cos(theta2pos))
+                
+                theta2_final = theta2pos
+                theta1_final = theta1pos
+                
+                angle_list.append([theta1pos, theta2pos, theta3pos])
+                
+        angle_list.append([theta1_final, theta2_final, up])
+            
 parse_image()
 
-# line, = ax.plot([], [], lw=2)
+# # call the animator.  blit=True means only re-draw the parts that have changed.
+# anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               # frames=frames, interval=interval, blit=False)
 
-for x_list, y_list in parse_list:
-    anim_line, = ax.plot([], [], lw=2)
+# plt.show()
 
-    line_list.append((x_list, y_list, anim_line))
-    
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=frames, interval=interval, blit=False)
+generate_arm_angles()
 
-plt.show()
+for angle_set in angle_list:
+    print angle_set
 
 
 
-
-
-
-
-
-
-
-    
     
     
     
